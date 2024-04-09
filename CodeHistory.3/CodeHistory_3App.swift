bed8aef7c56d7e215ccfadbdd5a453d8ddb9f33e
Alexis Portillo <alexisjpruz@gmail.com>
2024-04-09 08:25:02 -0400

    Initial Commit

---
+++ CodeHistory.3/CodeHistory_3App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistory_3App.swift
+//  CodeHistory.3
+//
+//  Created by Alexis Portillo on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistory_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
